7e047de1c84eba3de69ef574135c8278ba8aab6a
Documenter.jl <documenter@juliadocs.github.io>
2023-11-05 11:16:48 +0000

    build based on defe799

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MWE","category":"page"},{"location":"#MWE","page":"Home","title":"MWE","text":"","category":"section"}]
+}
